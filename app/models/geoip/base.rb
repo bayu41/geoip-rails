9ddd1c4c7fe1e7cd1851a5a4640bc545b56b968e
bayu41 <bayu@41studio.com>
2016-08-22 15:47:05 +0700

    Update base.rb

--- app/models/geoip/base.rb
+++ app/models/geoip/base.rb
@@ -3,7 +3,7 @@ module Geoip
     
     self.abstract_class = true
     
-    establish_connection(ActiveRecord::Base.configurations["geoip_#{::Rails.env}"])
+    establish_connection(ActiveRecord::Base.configurations["#{::Rails.env}"])
     
   end
 end
